af3745b49cbe9b438a618252c966be94a0e24ca8
takashi-koshiba <koshiba123451@gmail.com>
2025-01-25 22:43:12 +0900

    Update CalcCost.java

--- java/CalcCost.java
+++ java/CalcCost.java
@@ -1,19 +1,20 @@
 
+
 public class CalcCost {
 
 
-	public  static Integer maxCost(Integer len) {
+	public  static Integer maxCost(Integer len,boolean isShort) {
 		Integer cost= (len * (len + 1)) / 2;
 
-		return len>3?cost-len:cost;
+		return isShort?cost:cost-len;
 	}
 	
 
 	
 	public static Double getMaxCost(String target,Integer inputTotalCost
-			,String[] split) {
+			,String[] split,boolean isShort) {
 
-		Integer targetCost=maxCost(target.length());
+		Integer targetCost=maxCost(target.length(),isShort);
 		
 		Integer inputCountMatch=countMatch(split,target);
 		
@@ -27,7 +28,7 @@ public class CalcCost {
 		return result;
 		
 	}
-	public static String[] splitStr(String str,Integer totalCost) {
+	public static String[] splitStr(String str,Integer totalCost,boolean isShort) {
 		String arr[]=new String[totalCost];
 		
 		Integer index=0;
@@ -36,7 +37,7 @@ public class CalcCost {
 			for(Integer ii=i+1;ii<=strLen;ii++) {
 				
 				String subStr=str.substring(i,ii);
-				if(strLen>3 &&subStr.length()<=1) {
+				if(!isShort &&subStr.length()<=1) {
 					continue;
 				}
 				
